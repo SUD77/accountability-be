e20a4d947b1fb7f6147ee9f33f6786d7261f7593
SUD77 <jaiswalsudhanshu20@gmail.com>
2025-08-27 10:35:01 +0530

    refactor : removed old code

--- src/routes/logsRouter.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-// import { upsertLogEntry, listLogEntries } from "../controllers/logsController";
-
-const logsRouter = Router();
-// logsRouter.post("/log-entries", upsertLogEntry);
-// logsRouter.get("/log-entries", listLogEntries);
-
-export default logsRouter;
